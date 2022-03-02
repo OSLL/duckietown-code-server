2754a3d8fc313c1125372b64f5e931cf41f2b06f
stacylkv <stacylkv@gmail.com>
2022-03-02 21:18:57 +0700

    Split backend and frontend, remove unnecessary files

---
+++ frontend/config/extensions/helloworld/src/extension.ts
@@ -0,0 +1,35 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import fetch from 'node-fetch';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "helloworld" is now active!');
+
+	console.log(getResponse());
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', async () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from HelloWorld!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export async function getResponse() {
+	const response = await fetch('http://localhost:5000/todo/api/v1.0/tasks');
+	//console.log(response);
+	return await response.json();
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
