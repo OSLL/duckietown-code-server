fa16d272222355e0256507773ff4749b787f4ce4
evsudakov <evsudakov@yandex-team.ru>
2022-03-18 16:37:23 +0300

    dcs10: fixed tab

--- frontend/config/extensions/helloworld/src/web/testLog.ts
+++ frontend/config/extensions/helloworld/src/web/testLog.ts
@@ -2,12 +2,12 @@ import * as vscode from 'vscode';
 
 export class LogMsgTreeProvider implements vscode.TreeDataProvider<any> {
     private _onDidChangeTreeData: vscode.EventEmitter<any | undefined | void> = new vscode.EventEmitter<any | undefined | void>();
-	readonly onDidChangeTreeData: vscode.Event<any | undefined | void> = this._onDidChangeTreeData.event;
+    readonly onDidChangeTreeData: vscode.Event<any | undefined | void> = this._onDidChangeTreeData.event;
 
     private test = vscode.window.createOutputChannel("test");
 
-	constructor(private workspaceRoot: string | undefined) {
-	}
+    constructor(private workspaceRoot: string | undefined) {
+    }
     getTreeItem(element: any): vscode.TreeItem | Thenable<vscode.TreeItem> {
         return element;
     }
@@ -15,9 +15,9 @@ export class LogMsgTreeProvider implements vscode.TreeDataProvider<any> {
 		return Promise.resolve([]);
     }
 
-	refresh(): void {
+    refresh(): void {
         this.test.appendLine("Clicked");
         // @ts-ignore
-		this._onDidChangeTreeData.fire();        
-	}
+        this._onDidChangeTreeData.fire();        
+    }
 }
