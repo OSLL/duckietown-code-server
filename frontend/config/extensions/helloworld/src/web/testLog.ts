767d2827653a9cf9e0f5d39cd43603071f16bbad
evsudakov <evsudakov@yandex-team.ru>
2022-03-18 12:53:12 +0300

    dcs10: added tree container and log button

---
+++ frontend/config/extensions/helloworld/src/web/testLog.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode';
+
+export class LogMsgTreeProvider implements vscode.TreeDataProvider<any> {
+    private _onDidChangeTreeData: vscode.EventEmitter<any | undefined | void> = new vscode.EventEmitter<any | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<any | undefined | void> = this._onDidChangeTreeData.event;
+
+    private test = vscode.window.createOutputChannel("test");
+
+	constructor(private workspaceRoot: string | undefined) {
+	}
+    getTreeItem(element: any): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        return element;
+    }
+    getChildren(element?: any): vscode.ProviderResult<any[]> {
+		return Promise.resolve([]);
+    }
+
+	refresh(): void {
+        this.test.appendLine("Clicked");
+        // @ts-ignore
+		this._onDidChangeTreeData.fire();        
+	}
+}
