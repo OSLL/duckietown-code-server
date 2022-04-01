5aec263046f843c8b6f2a825e63c1f03a57cfdbe
Valentina-Gol <vip.valya.golubeva@mail.ru>
2022-04-01 14:13:11 +0300

    build try to work

--- src/solution/test_build.py
+++ src/solution/test_build.py
@@ -1,8 +1,11 @@
 import subprocess
 from pathlib import Path
 import logging
+import os
+#hostname = ""
+#dir = ""
 BUILD_COMMAND = "dts devel build --workdir {dir} -H {hostname}.local -f"
-COPY_COMMAND = "rsync --rsh=\"sshpass -p quackquack ssh -o StrictHostKeyChecking=no -l duckie\" --archive duckie@{hostname}.local:/code/template-ros-core/ {dir}"
+#COPY_COMMAND = f"rsync --rsh='sshpass -p quackquack ssh -o StrictHostKeyChecking=no -l duckie' --archive duckie@{hostname}.local:/code/template-ros-core/ {dir}"
 logging.basicConfig(level=logging.INFO)
 
 
@@ -10,13 +13,19 @@ def build_template_ros_core(hostname: str, directory: Path, log: Path) -> None:
     logging.info(f"BUILD template for hostname [{hostname}], directory [{directory}]")
     with open(log.absolute(), 'w') as file:
         # copy directory from bot to local machine
+        '''
         subprocess.Popen(COPY_COMMAND.format(
             hostname=hostname,
             dir=str(directory.absolute())
         ).split(), stdout=file, stderr=file)
+        '''
+        dir = str(directory.absolute())
+        COPY_COMMAND = f'rsync --rsh="sshpass -p quackquack ssh -o StrictHostKeyChecking=no -l duckie" --archive duckie@{hostname}.local:/code/template-ros-core/ {dir}'
+        os.system(COPY_COMMAND)
+        #subprocess.Popen(COPY_COMMAND.split())
 
-        file.write("Log for {}".format(hostname))
-        file.flush()
+        #file.write("Log for {}".format(hostname))
+        #file.flush()
         if not directory.is_absolute():
             logging.error(f"BUILD [{str(directory.absolute())}] is not absolute:")
             raise ValueError(f"[{str(directory.absolute())}] is not absolute:")
@@ -27,4 +36,4 @@ def build_template_ros_core(hostname: str, directory: Path, log: Path) -> None:
 
 
 if __name__ == '__main__':
-    build_template_ros_core("autobot01", Path("/home/i/template-ros-core"), Path("./logs.txt"))
+    build_template_ros_core("autobot07", Path("/home/i/template-ros-core"), Path("./logs.txt"))
