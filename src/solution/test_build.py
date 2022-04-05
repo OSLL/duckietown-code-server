0c08e8a4ac6c45cdbd89fe2aa00b6a4f9bd2f365
Valentina-Gol <vip.valya.golubeva@mail.ru>
2022-04-05 11:42:10 +0300

    ready to check

--- src/solution/test_build.py
+++ src/solution/test_build.py
@@ -2,10 +2,8 @@ import subprocess
 from pathlib import Path
 import logging
 import os
-#hostname = ""
-#dir = ""
+
 BUILD_COMMAND = "dts devel build --workdir {dir} -H {hostname}.local -f"
-#COPY_COMMAND = f"rsync --rsh='sshpass -p quackquack ssh -o StrictHostKeyChecking=no -l duckie' --archive duckie@{hostname}.local:/code/template-ros-core/ {dir}"
 logging.basicConfig(level=logging.INFO)
 
 
@@ -13,22 +11,15 @@ def build_template_ros_core(hostname: str, directory: Path, log: Path) -> None:
     logging.info(f"BUILD template for hostname [{hostname}], directory [{directory}]")
     with open(log.absolute(), 'w') as file:
         # copy directory from bot to local machine
-        '''
-        subprocess.Popen(COPY_COMMAND.format(
-            hostname=hostname,
-            dir=str(directory.absolute())
-        ).split(), stdout=file, stderr=file)
-        '''
+        logging.info(
+            f"COPY template dir from hostname [{hostname}]")
         dir = str(directory.absolute())
         COPY_COMMAND = f'rsync --rsh="sshpass -p quackquack ssh -o StrictHostKeyChecking=no -l duckie" --archive duckie@{hostname}.local:/code/template-ros-core/ {dir}'
         os.system(COPY_COMMAND)
-        #subprocess.Popen(COPY_COMMAND.split())
-
-        #file.write("Log for {}".format(hostname))
-        #file.flush()
         if not directory.is_absolute():
             logging.error(f"BUILD [{str(directory.absolute())}] is not absolute:")
             raise ValueError(f"[{str(directory.absolute())}] is not absolute:")
+        # build solution
         subprocess.Popen(BUILD_COMMAND.format(
             hostname=hostname,
             dir=str(directory.absolute())
--- src/solution/test_run.py
+++ src/solution/test_run.py
@@ -1,36 +1,26 @@
-from pathlib import Path
 import subprocess
+from pathlib import Path
 import logging
 import os
 
-logging.basicConfig(level=logging.INFO)
-
 RUN_COMMAND = "dts devel run --workdir {dir} -M -s -f -H {hostname} --"
-#COPY_COMMAND = "rsync --rsh=\"sshpass -p quackquack ssh -o StrictHostKeyChecking=no -l duckie\" --archive duckie@{hostname}.local:/code/template-ros-core/ {dir}"
 logging.basicConfig(level=logging.INFO)
 
 
 def run_template_ros_core(hostname: str, directory: Path, log: Path) -> None:
+    logging.info(f"RUN template for hostname [{hostname}], directory [{directory}]")
     with open(log.absolute(), 'w') as file:
         # copy directory from bot to local machine
         logging.info(
             f"COPY template dir from hostname [{hostname}]")
         dir = str(directory.absolute())
         COPY_COMMAND = f'rsync --rsh="sshpass -p quackquack ssh -o StrictHostKeyChecking=no -l duckie" --archive duckie@{hostname}.local:/code/template-ros-core/ {dir}'
-        print(COPY_COMMAND)
         os.system(COPY_COMMAND)
-
-        #file.write("Log for {}".format(hostname))
-        #file.flush()
         # run solution
-        logging.info(
-            f"RUN template for hostname [{hostname}], directory [{directory}]")
-
         subprocess.Popen(RUN_COMMAND.format(
             dir=str(directory.absolute()),
             hostname=hostname
         ).split(), stdout=file, stderr=file)
-    # send_start_command(hostname)
 
 
 if __name__ == '__main__':
