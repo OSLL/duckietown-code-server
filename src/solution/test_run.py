7847fde79c04ef913afef3d3f6f6b99af180d966
Valentina-Gol <vip.valya.golubeva@mail.ru>
2022-04-08 16:53:34 +0300

    build and run works

--- src/solution/test_run.py
+++ src/solution/test_run.py
@@ -1,27 +1,33 @@
-import subprocess
 from pathlib import Path
 import logging
 import os
+import subprocess
 
-RUN_COMMAND = "dts devel run --workdir {dir} -M -s -f -H {hostname} --"
 logging.basicConfig(level=logging.INFO)
 
 
 def run_template_ros_core(hostname: str, directory: Path, log: Path) -> None:
-    logging.info(f"RUN template for hostname [{hostname}], directory [{directory}]")
+    logging.info(f"BUILD template for hostname [{hostname}], directory [{directory}]")
     with open(log.absolute(), 'w') as file:
         # copy directory from bot to local machine
         logging.info(
             f"COPY template dir from hostname [{hostname}]")
         dir = str(directory.absolute())
         os.system(f'rm -fr {dir}/*')
+
         COPY_COMMAND = f'rsync --rsh="sshpass -p quackquack ssh -o StrictHostKeyChecking=no -l duckie" --archive duckie@{hostname}.local:/code/template-ros-core/ {dir}'
         os.system(COPY_COMMAND)
-        # run solution
+
+        RUN_COMMAND = "dts devel run --workdir {dir} -M -s -f -H {hostname} --"
+
+        print(RUN_COMMAND.format(
+            hostname=hostname,
+            dir=str(directory.absolute())
+        ).split())
         subprocess.Popen(RUN_COMMAND.format(
-            dir=str(directory.absolute()),
-            hostname=hostname
-        ).split(), stdout=file, stderr=file)
+            hostname=hostname,
+            dir=str(directory.absolute())
+        ).split())
 
 
 if __name__ == '__main__':
