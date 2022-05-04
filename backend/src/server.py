d8df8bbc5f4bb9c73f3e41844794b786746da435
Valentina-Gol <vip.valya.golubeva@mail.ru>
2022-05-04 23:20:42 +0300

    added stop request handler

--- backend/src/server.py
+++ backend/src/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from solution.test_run import run_template_ros_core
 from solution.test_build import build_template_ros_core
+from solution.test_stop import stop_template_ros_core
 from flask import jsonify
 
 app = Flask(__name__)
@@ -24,6 +25,15 @@ def run():
     hostname: str = request.args.get("hostname")
     run_template_ros_core(hostname)
     message = {'message': 'Run solution!'}
+    return jsonify(message)
+
+
+@app.route("/stop", methods=['GET'])
+#@cross_origin()
+def stop():
+    hostname = request.args.get("hostname")
+    stop_template_ros_core(hostname)
+    message = {'message': 'Stop solution!'}
     return jsonify(message)
 
 
