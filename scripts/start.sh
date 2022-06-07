1ed52412116e11cc0350713c0b8e39597d8fcf7f
light5551 <sergey.glazunov.99@mail.ru>
2022-06-07 14:14:54 +0300

    fixed nginx... & delete docker system prune

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ then
     docker stop web-ide-frontend web-ide-backend web-ide-nginx
     docker rm web-ide-frontend web-ide-backend web-ide-nginx
 
-    docker system prune -f -a
+    #docker system prune -f -a
     docker images -a
 
     docker-compose up --build
